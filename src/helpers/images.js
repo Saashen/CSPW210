37797c60b118e9037975034a1d714103f7fadcd2
Saashen <saashen.kyiv@gmail.com>
2021-07-03 02:56:39 +0200

    updated file size err

--- src/helpers/images.js
+++ src/helpers/images.js
@@ -4,19 +4,28 @@ const jimp = require('jimp');
 const cloudinary = require('cloudinary').v2;
 require('dotenv').config();
 
-const upload = multer({
-  dest: path.join(__dirname, '../..', 'uploads'),
-  limits: { fileSize: 2000000 },
-  fileFilter: (req, file, cb) => {
-    if (file.mimetype.includes('image')) {
-      cb(null, true);
-      return;
-    }
-    cb(new Error('Wrong file format'), false);
-  },
-});
+function uploadImage(req, res, next) {
+  const upload = multer({
+    dest: path.join(__dirname, '../..', 'uploads'),
+    limits: { fileSize: 2000000 },
+    fileFilter: (req, file, cb) => {
+      if (file.mimetype.includes('image')) {
+        cb(null, true);
+        return;
+      }
+      cb(new Error('Wrong file format'), false);
+    },
+  }).single('image');
 
-const uploadImage = upload.single('image');
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError && err.code === 'LIMIT_FILE_SIZE') {
+      return res.status(400).send({
+        message: 'File size should be up to 2 MB',
+      });
+    }
+    next();
+  });
+}
 
 const resizeImage = pathFile =>
   jimp.read(pathFile).then(img => img.quality(60).writeAsync(pathFile));
